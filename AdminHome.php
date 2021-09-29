e3115aa3d23b3e3801357bedc97baf89028c6b7d
Mask13 <aaron.r.samuel@gmail.com>
2021-09-28 23:37:50 -0400

    Update AdminHome.php

--- AdminHome.php
+++ AdminHome.php
@@ -2,7 +2,7 @@
 session_start();
 require ("config.php");
 if(isset($_SESSION['id'])){
-  if($_SESSION['id'] == 3){
+  if($_SESSION['IsAdmiin']== 1){
     echo 'hello admin';
   }
   else{
@@ -18,6 +18,9 @@ else{
   <head>
     <meta charset="utf-8">
     <title>Admin</title>
+    <button type="button" onclick="location.href = 'https://web.njit.edu/~as3655/CS490/Logout.php';"
+           class = "button" name="Login"> Logout
+   </button>
   </head>
   <style>
       body{
@@ -38,29 +41,35 @@ else{
               text-decoration: none;
               display: inline-block;
               font-size: 16px;
-            }
-          .formInput1{
-            width: 40%;
-            padding: 10px;
-            border: 2px solid #c6a226;
-            border-radius: 1px;
-            box-sizing: border-box;
-            resize: vertical;
-          }
-          .formInput2{
-            width: 40%;
-            padding: 10px;
-            border: 2px solid #c6a226;
-            border-radius: 1px;
-            box-sizing: border-box;
-            resize: vertical;
           }
           .label {
             padding: 12px 12px 12px 0;
             display: inline-block;
           }
+          .titles{
+            width: 200px;
+            text-align: center;
+            font-size: 40;
+            font-family: Trebuchet MS;
+            text-decoration-color:#c6a226;
+            border-bottom: 5px solid #c6a226;
+            border-top: 5px solid #c6a226;
+            padding: 2px;
+          }
  </style>
   <body>
+    <!-- Tests section -->
+    <center><header class=titles>Tests</header></center>
+    <button type="button" onclick="location.href = 'https://web.njit.edu/~as3655/CS490/Logout.php';"
+           class = "button" name="MNTest"> Make New Test
+   </button>
+   <!-- display current tests that are made here -->
 
+   <!-- Questons -->
+   <center><header class=titles>Questons</header></center>
+   <button type="button" onclick="location.href = 'https://web.njit.edu/~as3655/CS490/Logout.php';"
+          class = "button" name="MNTest"> Make New Question
+  </button>
+  <!-- display current questions that in the bank here -->
   </body>
 </html>
