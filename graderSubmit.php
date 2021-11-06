362b0e6b607ff443ee3d079d659f5c4757053df9
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-06 12:35:56 -0400

    commit on Candidate-Release

--- graderSubmit.php
+++ graderSubmit.php
@@ -11,11 +11,16 @@ $questions = array("Q1", "Q2", "Q3", "Q4", "Q5");
 foreach ($questions as $qNum) {
     if (isset($_POST["B1$qNum"])) {
         // updating points
+        $s = $db->prepare("SELECT $value FROM QuestionAssignments WHERE EID = '$EID'");
+        $s-> execute();
+        $r = $s->fetch(PDO::FETCH_ASSOC);
+        $qID = $r["$value"];
+
         $s = $db->prepare("UPDATE answers SET QP = '$_POST['B1$qNum']' WHERE QuestionID = '$qID'");
         $r = $s->execute();
 
         $qPoints = $_POST["B1$qNum"];
-        //header("Refresh:0; url=autograde.php");
+        header("Refresh:0; url=autograde.php");
     }
 
     //  IF $_POST["B2$qNum"] {
