6fd1a43bf816e194801094178b0ecab183625445
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-06 11:32:37 -0400

    commit on Candidate-Release

--- graderSubmit.php
+++ graderSubmit.php
@@ -13,11 +13,17 @@ include "autograde.php";
 
 $questions = array("Q1", "Q2", "Q3", "Q4", "Q5");
 foreach ($questions as $qNum) {
-    //  IF $_POST["B1$qNum"] {
+      if ($_POST["B1$qNum"] == $qPoints) {
 
-    //      change the score in autograder 
-    //      then refresh
-    //  }
+        // updating points
+        $s = $db->prepare("UPDATE answers SET QP = '$_POST['B1$qNum']' WHERE questionID = '$qID'");
+        $r = $s->execute();
+
+        $qPoints = $_POST["B1$qNum"];
+        header("Refresh:0");
+        // change the score in autograder 
+        // then refresh
+    }
 
     //  IF $_POST["B2$qNum"] {
 
