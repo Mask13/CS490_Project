246f74215eed6c7310932c1fc43e5a665f3afe87
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-06 15:27:00 -0400

    commit on Candidate-Release

--- graderSubmit.php
+++ graderSubmit.php
@@ -23,6 +23,7 @@ foreach ($questions as $qNum) {
         $params = array(":QP" => $_POST["qPoints$qNum"]);
         $r = $s->execute($params);
 
+        echo "$_POST['qPoints$qNum']";
         $qPoints = $_POST["qPoints$qNum"];
         header("Location = autograde.php");
     }
