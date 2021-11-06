ca25fba3ca4d40ae0557caf451485918671815cc
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-06 12:09:24 -0400

    commit on Candidate-Release

--- graderSubmit.php
+++ graderSubmit.php
@@ -1,12 +1,8 @@
 <?php
 
-require "config.php";
+require ("config.php");
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-$db = new PDO($connection_string, $dbuser, $dbpass);
-try {
-  $sql = $db->prepare("SELECT Username from users Where IsAdmin = 0");
-  $sql->execute();
-}
+$db= new PDO($connection_string, $dbuser, $dbpass);
 finally{}
 
 include "autograde.php"; 
@@ -15,13 +11,11 @@ $questions = array("Q1", "Q2", "Q3", "Q4", "Q5");
 foreach ($questions as $qNum) {
     if (isset($_POST["B1$qNum"])) {
         // updating points
-        $s = $db->prepare("UPDATE answers SET QP = '$_POST['B1$qNum']' WHERE questionID = '$qID'");
+        $s = $db->prepare("UPDATE answers SET QP = '$_POST['B1$qNum']' WHERE QuestionID = '$qID'");
         $r = $s->execute();
 
         $qPoints = $_POST["B1$qNum"];
-        header("Refresh:0; url=autograde.php");
-        // change the score in autograder 
-        // then refresh
+        //header("Refresh:0; url=autograde.php");
     }
 
     //  IF $_POST["B2$qNum"] {
