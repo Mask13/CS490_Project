b5dabdeb2537b613082fb499f761e78a8641a2c4
Mask13 <aaron.r.samuel@gmail.com>
2021-11-11 15:20:14 -0500

    Update TestTake.php

--- TestTake.php
+++ TestTake.php
@@ -52,6 +52,7 @@ else{
         background-color: #000033;
         background-image: url('https://images.unsplash.com/photo-1445905595283-21f8ae8a33d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80');
         background-repeat: repeat;
+        height: auto;
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
