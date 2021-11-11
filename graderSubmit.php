da1f0cb326a0b5c356d7defed87c34f84ba9dc64
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-11 17:00:42 -0500

    style edit

--- graderSubmit.php
+++ graderSubmit.php
@@ -6,6 +6,20 @@
 </button>
 </header>
 
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-radius: 10px;
+    border-color: #c6a226;
+    color: white;
+    padding: 2px;
+    }
+    th, td {
+    background-color: black;
+    }
+</style>
+
+
 <body>
 <h2>Results of AutoGrader</h2>
 
@@ -35,11 +49,6 @@ foreach ($questions as $qNum) {
     $value = $qNum."P";
 
     if ($r["$qNum"] != NULL && $r["$qNum"] != 0) {
-
-        echo "<style>";
-        echo " table, th, td {";
-        echo " border:1px solid black;}";
-        echo "</style>";
         
         echo "<br>";
         echo "<table style='width:100%'>";
