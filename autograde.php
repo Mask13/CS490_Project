45b79d892c14586228796602aae28f1b9c820ee6
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-11 16:29:40 -0500

    tcp calculations part 3

--- autograde.php
+++ autograde.php
@@ -138,19 +138,6 @@ foreach ($questions as $value) {
           $s = $db->prepare("UPDATE answers SET $testNum = 0 WHERE QuestionID = '$qID' and resultID = '$reID'");
           $r = $s->execute();
         }
-
-        $testNum = "TCP".$x;
-        $s = $db->prepare("SELECT $testNum FROM answers WHERE QuestionID = '$qID' and resultID = '$reID'");
-        $s->execute();
-        $r = $s->fetch(PDO::FETCH_ASSOC);
-        $testCaseStat = $r["$testNum"];
-
-        // adding actual test case points
-        if ($testCaseStat = 1) {
-          $s = $db->prepare("UPDATE answers SET $testNum = '$tcPoints' WHERE QuestionID = '$qID' and resultID = '$reID'");
-          $r = $s->execute();
-        }
-        else {}
       }
     }
     // =======================================================
@@ -242,21 +229,18 @@ foreach ($questions as $value) {
       $r = $s->fetch(PDO::FETCH_ASSOC);
       $expAnswer = $r["$aInput00"];
 
-      /*$testNum = "TCP".$x;
+      $testNum = "TCP".$x;
       $s = $db->prepare("SELECT $testNum FROM answers WHERE QuestionID = '$qID' and resultID = '$reID'");
       $s->execute();
       $r = $s->fetch(PDO::FETCH_ASSOC);
       $testCaseStat = $r["$testNum"];
 
       // adding actual test case points
-      if ($testCaseStat = 1) {
+      if ($testCaseStat == 1) {
         $s = $db->prepare("UPDATE answers SET $testNum = '$tcPoints' WHERE QuestionID = '$qID' and resultID = '$reID'");
         $r = $s->execute();
       }
-      else {
-        $s = $db->prepare("UPDATE answers SET $testNum = 0 WHERE QuestionID = '$qID' and resultID = '$reID'");
-        $r = $s->execute();
-      }*/
+      else {}
 
     }
 
