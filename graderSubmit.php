0d2d4ce33a0f4c9f7778bf23abdcb4ef66cfbacb
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-13 13:54:43 -0500

    graderSubmit calc part 7

--- graderSubmit.php
+++ graderSubmit.php
@@ -299,14 +299,16 @@ if (!empty($_POST)) {
 
                 if ($testPoints != NULL) {
                     $STP += $testPoints;
-                }
 
-                $s = $db->prepare("UPDATE answers SET STP = '$STP' WHERE resultID = '$reID' and QuestionID = '$qID'");
-                $r = $s->execute();
+                    $s = $db->prepare("UPDATE answers SET STP = '$STP' WHERE resultID = '$reID' and QuestionID = '$qID'");
+                    $r = $s->execute();
+                }
+                else {
+                    break;
+                }
 
                 // break;
             }
-            break;
         }
     }
     echo("<meta http-equiv='refresh' content='1'>");
