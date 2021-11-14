3245c873ba3e61bc3c1112eb9cbc584d476af10e
Mask13 <aaron.r.samuel@gmail.com>
2021-11-14 12:26:47 -0500

    Update Questions.php

--- Questions.php
+++ Questions.php
@@ -16,45 +16,56 @@
   </head><br>
   <style>
       text{
-          font-size: 20px;
+        font-size: 20px;
       }
       br {
-          line-height: 250%;
-          }
+        line-height: 250%;
+      }
+      table, th, td {
+        border: 1px solid black;
+        border-radius: 10px;
+        border-color: #c6a226;
+        color: white;
+        padding: 2px;
+        padding-bottom: 6px;
+      }
+      th, td {
+        background-color: black;
+      }
       body{
-           background-color: #000033;
-           background-image: url('https://images.unsplash.com/photo-1445905595283-21f8ae8a33d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80');
-           height: 100%;
-           background-position: center;
-           background-repeat: no-repeat;
-           background-size: cover;
-           color: #bcbdbe;
-           }
-					 .button {
-              background-color: Transparent;
-              border: inset #c6a226;
-              color: #bcbdbe;
-              padding: 15px 19px;
-              text-align: center;
-              text-decoration: none;
-              display: inline-block;
-              border-radius: 25px;
-              font-size: 16px;
-          }
-          .label {
-            padding: 12px 12px 12px 0;
-            display: inline-block;
-          }
-          titles{
-            width: 200px;
-            text-align: center;
-            font-size: 40;
-            font-family: Trebuchet MS;
-            text-decoration-color:#c6a226;
-            border-bottom: 5px solid #c6a226;
-            border-top: 5px solid #c6a226;
-            padding: 2px;
-          }
+        background-color: #000033;
+        background-image: url('https://images.unsplash.com/photo-1445905595283-21f8ae8a33d2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80');
+        height: 100%;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        color: #bcbdbe;
+      }
+      .button {
+        background-color: Transparent;
+        border: inset #c6a226;
+        color: #bcbdbe;
+        padding: 15px 19px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        border-radius: 25px;
+        font-size: 16px;
+      }
+      .label {
+        padding: 12px 12px 12px 0;
+        display: inline-block;
+      }
+      titles{
+        width: 200px;
+        text-align: center;
+        font-size: 40;
+        font-family: Trebuchet MS;
+        text-decoration-color:#c6a226;
+        border-bottom: 5px solid #c6a226;
+        border-top: 5px solid #c6a226;
+        padding: 2px;
+      }
  </style>
   <body>
     <!-- Tests section -->
