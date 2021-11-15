dbf27f7a2a788343f8900e5b2bde1bc4266c18db
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-15 12:56:16 -0500

    userhome button changes

--- UserHome.php
+++ UserHome.php
@@ -80,6 +80,34 @@ else{
               border-radius: 25px;
               font-size: 16px;
           }
+          .button:hover {background-color: rgb(69, 74, 28);}
+
+          .button:active {
+            background-color: rgb(69, 74, 28);
+            box-shadow: 0 5px #666;
+            transform: translateY(4px);
+          }
+          .button3 {
+            display: inline-block;
+            padding: 15px 25px;
+            font-size: 24px;
+            text-align: center;
+            cursor: pointer;
+            outline: none;
+            color: #bcbdbe;
+            background-color: rgb(230, 231, 208);
+            border: inset #c6a226;
+            border-radius: 15px;
+            box-shadow: 0 9px #999;
+            font-size: 16px;
+          }
+          .button3:hover {background-color: rgb(69, 74, 28);}
+
+          .button3:active {
+            background-color: rgb(69, 74, 28);
+            box-shadow: 0 5px #666;
+            transform: translateY(4px);
+          }
           .label {
             padding: 12px 12px 12px 0;
             display: inline-block;
