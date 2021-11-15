e45fc878d064d127876124126f7594eb30ffddb1
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-14 21:41:42 -0500

    spacing mess

--- AdminHome.php
+++ AdminHome.php
@@ -102,75 +102,82 @@ else{
         <!-- Tests section -->
         <button type="button" onclick="location.href = 'Tests.php';"
               class = "button" name="MNTest"> View Tests
-      </button>
-      <!-- Questons -->
-      <button type="button" onclick="location.href = 'Questions.php';"
-              class = "button" name="MNTest"> View Questions
-      </button>
-      <!-- display current students here-->
-      <center><titles style="position:relative; top:60">Students</titles></center>
-      <form method="post">
-          <?php
-            require "config.php";
-            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-            $db= new PDO($connection_string, $dbuser, $dbpass);
-            try{
-              $sql ="SELECT UID, Username from users Where IsAdmin = 0";
-
-              echo "<select id='studentID' name='studentID' value=''>Student Name</option>"; // list box select command
-
-              foreach ($db->query($sql) as $row){//Array or records stored in $row
-                echo "<option value=$row[UID]>$row[Username]</option>";
-              }
-              echo "</select>";// Closing of list box
-            }
-            finally{}
-        ?>
-        <input class= "button" type="submit" value="See tests"/>
-      </form>
+        </button>
+      </div>
     </div>
-   </div> 
+    <!-- Questons -->
+    <div class="container">
+      <div class="center">
+        <button type="button" onclick="location.href = 'Questions.php';"
+                class = "button" name="MNTest"> View Questions
+        </button>
+        <!-- display current students here-->
+        <center><titles style="position:relative; top:60">Students</titles></center>
+        <form method="post">
+            <?php
+              require "config.php";
+              $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+              $db= new PDO($connection_string, $dbuser, $dbpass);
+              try{
+                $sql ="SELECT UID, Username from users Where IsAdmin = 0";
+
+                echo "<select id='studentID' name='studentID' value=''>Student Name</option>"; // list box select command
+
+                foreach ($db->query($sql) as $row){//Array or records stored in $row
+                  echo "<option value=$row[UID]>$row[Username]</option>";
+                }
+                echo "</select>";// Closing of list box
+              }
+              finally{}
+            ?>
+            <input class= "button" type="submit" value="See tests"/>
+        </form>
+      </div>
+    </div> 
   </body>
 </html>
 
 <html>
+<body>
 
-<?php
-  if(isset($_POST["Request_Test"]) || isset($_POST["studentID"])){
-    GetTests();
-  }
-
-  function GetTests(){
-    require "config.php";
-    $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-    $db= new PDO($connection_string, $dbuser, $dbpass);
-    try{
-      $sql = "SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'";
-      echo "<table>"; // list box select command
-      echo "<tr>";
-      echo "<th>Exam Name</th>";
-      echo "<th>Result</th>";
-      echo "<th>Comment</th>";
-      echo "<th>ResultID</th>";
-      echo "</tr>";
-      foreach ($db->query($sql) as $row){//Array or records stored in $row
-        $sql2 = $db->prepare("SELECT Exam_Name from exams Where EID = '$row[EID]'");
-        $sql2->execute();
-        $row2 = $sql2->fetch(PDO::FETCH_ASSOC);
-        echo "<tr>";
-        echo "<td>$row2[Exam_Name]</td>";
-        echo "<td>$row[result]</td>";
-        echo "<td>$row[comments]</td>";
-        echo "<td>$row[resultID]</td>";
-        echo "</tr>";
+<div class="container">
+  <div class="center">
+    <?php
+      if(isset($_POST["Request_Test"]) || isset($_POST["studentID"])){
+        GetTests();
       }
-      echo "</table>";// Closing of list box
-      $sql = $db->prepare("SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'");
-      $sql->execute();
-      $glob = $sql->fetch(PDO::FETCH_ASSOC);
-    }
-    finally{}
-  }
- ?>
 
- /
+      function GetTests(){
+        require "config.php";
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        $db= new PDO($connection_string, $dbuser, $dbpass);
+        try{
+          $sql = "SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'";
+          echo "<table>"; // list box select command
+          echo "<tr>";
+          echo "<th>Exam Name</th>";
+          echo "<th>Result</th>";
+          echo "<th>Comment</th>";
+          echo "<th>ResultID</th>";
+          echo "</tr>";
+          foreach ($db->query($sql) as $row){//Array or records stored in $row
+            $sql2 = $db->prepare("SELECT Exam_Name from exams Where EID = '$row[EID]'");
+            $sql2->execute();
+            $row2 = $sql2->fetch(PDO::FETCH_ASSOC);
+            echo "<tr>";
+            echo "<td>$row2[Exam_Name]</td>";
+            echo "<td>$row[result]</td>";
+            echo "<td>$row[comments]</td>";
+            echo "<td>$row[resultID]</td>";
+            echo "</tr>";
+          }
+          echo "</table>";// Closing of list box
+          $sql = $db->prepare("SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'");
+          $sql->execute();
+          $glob = $sql->fetch(PDO::FETCH_ASSOC);
+        }
+        finally{}
+      }
+    ?>
+  </div>
+</div>
