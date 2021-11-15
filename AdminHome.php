01a973a52258f36420eadb9036fc737a40e880c4
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-15 10:33:02 -0500

    margin left

--- AdminHome.php
+++ AdminHome.php
@@ -134,7 +134,7 @@ else{
           }
           container3 {
             margin: 30px;
-            margin-right:20px;
+            margin-left:20px;
             padding: 10px;
             height: 100px;
             position: relative;
