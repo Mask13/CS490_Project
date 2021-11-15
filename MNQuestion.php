b62a1a46a68083694d16a3753895428db3914c4c
Mask13 <aaron.r.samuel@gmail.com>
2021-11-15 13:58:08 -0500

    style change

--- MNQuestion.php
+++ MNQuestion.php
@@ -264,7 +264,7 @@
    <br>
    <br>
    <form class = "formNQ" name="NewQuestion" id="myForm" method="POST">
-     <textarea form = 'formNQ' type= "Text" name = "QT" id="QT" placeholder="Question Text"></textarea><br>
+     <textarea form = 'formNQ' class= "formInput1" type= "Text" name = "QT" id="QT" placeholder="Question Text"></textarea><br>
      <!-- Info for test case 1 -->
      <input class= "formInput1" type= "Text" name = "QI1" id="QI1" placeholder="Question Test 1"></input><br>
      <input class= "formInput1" type= "Text" name = "QA1" id="QA1" placeholder="Answer 1"></input><br>
