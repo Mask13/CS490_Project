ec2010d1751ce36a0b12a788a2764838d8af01ee
Mask13 <aaron.r.samuel@gmail.com>
2021-11-14 21:20:57 -0500

    Update Tests.php

--- Tests.php
+++ Tests.php
@@ -203,6 +203,6 @@
     if(isset($_POST["studentID"]) && isset($_POST["EID"])){
       $_SESSION["EID"] = $_POST["EID"];
       $_SESSION["SID"] = $_POST["studentID"];
-      header("Location: autograde.php");
+      echo "<html><script> window.location.href = 'autograde.php'; </script></html>";
     }
  ?>
