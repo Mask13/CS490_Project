8ad1dea09408ba181ce68514533ff471b54221bb
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-15 09:18:29 -0500

    table fix

--- AdminHome.php
+++ AdminHome.php
@@ -199,40 +199,75 @@ else{
   </body>
 </html>
 
-<?php
-  if(isset($_POST["Request_Test"]) || isset($_POST["studentID"])){
-    GetTests();
-  }
+<html>
+<body>
 
-  function GetTests(){
-    require "config.php";
-    $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-    $db= new PDO($connection_string, $dbuser, $dbpass);
-    try{
-      $sql = "SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'";
-      echo "<table>"; // list box select command
-      echo "<tr>";
-      echo "<th>Exam Name</th>";
-      echo "<th>Result</th>";
-      echo "<th>Comment</th>";
-      echo "<th>ResultID</th>";
-      echo "</tr>";
-      foreach ($db->query($sql) as $row){//Array or records stored in $row
-        $sql2 = $db->prepare("SELECT Exam_Name from exams Where EID = '$row[EID]'");
-        $sql2->execute();
-        $row2 = $sql2->fetch(PDO::FETCH_ASSOC);
-        echo "<tr>";
-        echo "<td>$row2[Exam_Name]</td>";
-        echo "<td>$row[result]</td>";
-        echo "<td>$row[comments]</td>";
-        echo "<td>$row[resultID]</td>";
-        echo "</tr>";
-      }
-      echo "</table>";// Closing of list box
-      $sql = $db->prepare("SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'");
-      $sql->execute();
-      $glob = $sql->fetch(PDO::FETCH_ASSOC);
+<style>
+  body{
+    background-color: #000033;
+    background-image: url('https://i0.wp.com/gifimage.net/wp-content/uploads/2017/08/space-animated-gif-9.gif');
+    height: auto;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    color: #bcbdbe;
     }
-    finally{}
-  }
-?>
+    .center {
+      margin: 30px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+    }
+    .container2 {
+      margin: 30px;
+      padding: 10px;
+      height: 100px;
+      position: relative;
+    }
+</style>
+<div class="container2">
+  <div class="center">
+  <?php
+    if(isset($_POST["Request_Test"]) || isset($_POST["studentID"])){
+      GetTests();
+    }
+
+    function GetTests(){
+      require "config.php";
+      $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+      $db= new PDO($connection_string, $dbuser, $dbpass);
+      try{
+        $sql = "SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'";
+        echo "<table>"; // list box select command
+        echo "<tr>";
+        echo "<th>Exam Name</th>";
+        echo "<th>Result</th>";
+        echo "<th>Comment</th>";
+        echo "<th>ResultID</th>";
+        echo "</tr>";
+        foreach ($db->query($sql) as $row){//Array or records stored in $row
+          $sql2 = $db->prepare("SELECT Exam_Name from exams Where EID = '$row[EID]'");
+          $sql2->execute();
+          $row2 = $sql2->fetch(PDO::FETCH_ASSOC);
+          echo "<tr>";
+          echo "<td>$row2[Exam_Name]</td>";
+          echo "<td>$row[result]</td>";
+          echo "<td>$row[comments]</td>";
+          echo "<td>$row[resultID]</td>";
+          echo "</tr>";
+        }
+        echo "</table>";// Closing of list box
+        $sql = $db->prepare("SELECT EID, result, comments, newGrade, resultID from results Where UID = '$_POST[studentID]'");
+        $sql->execute();
+        $glob = $sql->fetch(PDO::FETCH_ASSOC);
+      }
+      finally{}
+    }
+  ?>
+  </div>
+</div>
+
+</body>
+</html>
