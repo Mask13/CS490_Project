1a6e696b34cf2b1a51dbcf796e7a390e09cf589c
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-16 15:26:31 -0500

    added jDeliver header

--- AdminHome.php
+++ AdminHome.php
@@ -22,8 +22,11 @@ else{
     <title>Jumbotron Template · Bootstrap</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/jumbotron/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
     <!-- Bootstrap core CSS -->
+    
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
