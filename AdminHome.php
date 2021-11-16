f44a717161942a237f3f637ad158a2cb5b3cc0b9
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-16 15:27:12 -0500

    added script to body

--- AdminHome.php
+++ AdminHome.php
@@ -49,6 +49,7 @@ else{
     <link href="jumbotron.css" rel="stylesheet">
   </head>
   <body>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
