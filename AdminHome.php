e20afebf8e268726917bce47898bf9a59ed18982
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-16 19:02:08 -0500

    Logout works now

--- AdminHome.php
+++ AdminHome.php
@@ -50,9 +50,9 @@ else{
   </head>
   <body>
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" style="display: inline-flex;">
   <a class="navbar-brand" href="#">CSClasses</a>
-  <form action="Logout.php" class="form-inline my-2 my-lg-0">
+  <form action="Logout.php" class="form-inline my-2 my-lg-0" style="display: inline-flex; margin-inline-start: auto;">
     <button class="btn btn-outline-success my-2 my-sm-0" style="background-color: rgb(230, 231, 208); " type="submit" href="Logout.php">Logout</button>
   </form>
 </nav>
