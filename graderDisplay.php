ffab4fe3b2334d1dda00dae2295ea2da8046e75d
j1nnnn <79156255+j1nnnn@users.noreply.github.com>
2021-11-22 10:10:32 -0500

    changed graderDisplay

--- graderDisplay.php
+++ graderDisplay.php
@@ -59,38 +59,29 @@
 </style>
 <body>
 <titles>Results of AutoGrader</titles>
-<form style="margin-left: -20px;" method="post">
+<form method="post">
     <?php
-        require "config.php";
+        include "config.php";
         $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         $db= new PDO($connection_string, $dbuser, $dbpass);
         try{
+        $sql = "SELECT EID, Exam_Name from exams";
+        echo "<select class='formInput1' id='VtestID' name='VtestID' value=''>Tests</option>"; // list box select command
+        foreach ($db->query($sql) as $row){//Array or records stored in $row
+            echo "<option value=$row[EID]>$row[Exam_Name]</option>";
+        }
+        echo "</select>";// Closing of list box
+
         $sql ="SELECT UID, Username from users Where IsAdmin = 0";
-
-        echo "<select class = 'formInput1' id='studentID' name='studentID'>Student Name</option>"; // list box select command
-
+        echo "<select class= 'select' id='VstudentID' name='VstudentID' value=''>Student Name</option>"; // list box select command
         foreach ($db->query($sql) as $row){//Array or records stored in $row
             echo "<option value=$row[UID]>$row[Username]</option>";
         }
         echo "</select>";// Closing of list box
         }
         finally{}
-        require "config.php";
-        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-        $db= new PDO($connection_string, $dbuser, $dbpass);
-        try{
-        $sql ="SELECT EID, Exam_Name from exams";
-
-        echo "<select class = 'formInput1' id='EID' name='EID'>Test</option>"; // list box select command
-
-        foreach ($db->query($sql) as $row){//Array or records stored in $row
-            echo "<option value=$row[EID]>$row[Exam_Name]</option>";
-        }
-        echo "</select>";// Closing of list box
-        }
-        finally{}
     ?>
-    <input class = "button" type= "submit" name = "autograde" id="autograde" value="autograde"></input>
+    <input class="btn btn-secondary" type="submit" value="Visit &raquo;"></input>
 </form>
 
 <?php
